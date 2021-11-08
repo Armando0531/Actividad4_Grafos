376b353eb9e7778d7950d11af140af0fe863d533
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-11-08 15:55:27 -0600

    Se agrego las clases grafoMatriz, Nodo, ColaLista

--- src/PruebaGrafos.java
+++ src/PruebaGrafos.java
@@ -37,6 +37,165 @@ class Vertice {
       
 }
 
+class GrafoMatriz{
+    int numVerts;
+    static int maxVerts;
+    Vertice [] verts;
+    int [][] matAd;
+    
+    public GrafoMatriz(){
+    	this(maxVerts);
+    }
+    public GrafoMatriz(int mx){
+    	maxVerts=mx;
+        matAd = new int [mx][mx];
+        verts = new Vertice[mx];
+        for (int i = 0; i < mx; i++)
+        	for (int j = 0; i < mx; i++)
+        		matAd[i][j] = 0;
+        numVerts = 0;
+    }
+    
+    public int numVertice(String vs) {
+         Vertice v = new Vertice(vs);
+         boolean encontrado = false;
+         int i = 0;
+         for (; (i < numVerts) && !encontrado;){
+        	 encontrado = verts[i].equals(v);
+        	 if (!encontrado) 
+        		 i++ ;
+        	 }
+         return (i < numVerts) ? i : -1 ;
+    }
+    public void nuevoVertice (String nom){
+    	boolean esta = numVertice(nom) >= 0;
+    	if (!esta){
+    		Vertice v = new Vertice(nom);
+    		v.asigVert(numVerts);
+    		verts[numVerts++] = v;
+    	}
+    }
+    public void nuevoArco(String a, String b)throws Exception{
+    	int va, vb;
+        va = numVertice(a);
+        vb = numVertice(b);
+        if (va < 0 || vb < 0) throw new Exception ("Vértice no existe");
+        matAd[va][vb] = 1;
+    }
+    public boolean adyacente(String a, String b)throws Exception{
+    	int va, vb;
+        va = numVertice(a);
+        vb = numVertice(b);
+        if (va < 0 || vb < 0) throw new Exception ("Vértice no existe");
+        return matAd[va][vb] == 1;
+    }
+    public static int[]recorrerAnchura(GrafoMatriz g, String org) throws Exception{
+    	int w, v;
+        int [] m;
+     
+        v = g.numVertice(org);
+        
+        int CLAVE =-1;
+        if (v < 0) throw new Exception("Vértice origen no existe");
+        
+        ColaLista cola = new ColaLista();
+        m = new int[g.numVerts];
+        // inicializa los vértices como no marcados
+        for (int i = 0; i < g.numVerts; i++)
+        m[i] = CLAVE;
+        m[v] = 0; // vértice origen queda marcado
+        cola.insertar(new Integer(v));
+        while (! cola.colaVacia()){
+        	Integer cw;
+        	cw = (Integer) cola.quitar();
+        	w = cw.intValue();
+        	System.out.println("Vértice " + g.verts[w] + "visitado");
+        	// inserta en la cola los adyacentes de w no marcados
+        	for (int u = 0; u < g.numVerts; u++)
+        	if ((g.matAd[w][u] == 1) && (m[u] == CLAVE))
+        	{
+        	// se marca vertice u con número de arcos hasta el
+        	m[u] = m[w] + 1;
+        	cola.insertar(new Integer(u));
+        	}
+        }
+        return m;
+        }
+    
+}
+
+class Nodo {
+	Object elemento;
+	Nodo siguiente;
+	int dato;
+	
+	public Nodo(Object x){
+		elemento = x;
+		siguiente = null;
+		}
+	public Nodo(int x){
+		dato = x;
+	    siguiente = null;
+	}
+	public Nodo(int x, Nodo n){
+	    dato = x;
+	    siguiente = n;
+	}
+	
+	public int getDato(){
+	    return dato;
+	}
+	public Nodo getEnlace(){
+	    return siguiente;
+	}
+	public void setEnlace(Nodo enlace){
+	    this.siguiente = enlace;
+	}
+}
+
+class ColaLista { 
+	protected Nodo frente;
+	protected Nodo fin;
+	
+	public ColaLista(){
+		frente = fin = null;
+	}
+	
+    public void insertar(Object elemento){
+    	Nodo a;
+        a = new Nodo(elemento);
+        if (colaVacia()){
+        	frente = a;
+        	}else{
+        		fin.siguiente = a;
+        	}
+        fin = a;
+        }
+    public Object quitar() throws Exception{
+    	Object aux;
+    	if (!colaVacia()){
+    		aux = frente.elemento;
+    		frente = frente.siguiente;
+    	}else
+    		throw new Exception("Eliminar de una cola vacía");
+    	return aux;
+    }
+    public void borrarCola(){
+    	for (; frente != null;){
+    		frente = frente.siguiente;
+        }
+    	System.gc();
+    }
+    public Object frenteCola() throws Exception{
+    	if (colaVacia()){
+    		throw new Exception("Error: cola vacía");
+        }
+    	return (frente.elemento);
+    }
+    public boolean colaVacia(){
+    	return (frente == null);
+    	}
+}
 
 public class PruebaGrafos {
 	public static void main(String[] args) {
